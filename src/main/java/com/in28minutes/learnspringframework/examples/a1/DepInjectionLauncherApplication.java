4f0150c7df9bbdaffb3a0e9c6a4f735bff174b73
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2023-12-27 12:30:52 +0900

    섹션 03: Step 04 - Exploring Spring Framework - Different Types of Dependency Injection -> recommended Constructor Injection

---
+++ src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
@@ -0,0 +1,87 @@
+package com.in28minutes.learnspringframework.examples.a1;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+/**
+ * author : ms.Lee
+ * date   : 2023-12-21
+ */
+
+// class 직접 만들어서 진행
+
+// Your Business Class
+@Component
+class YourBusinessClass {
+
+  Dependency1 dependency1;
+
+  Dependency2 dependency2;
+
+  /**
+   *
+   * recommended Injection
+   */
+  // @Autowired - 생성자는 @Autowired 가 필수가 아님.
+  public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+
+    System.out.println("Constructor Injection - YourBusinessClass");
+    this.dependency1 = dependency1;
+    this.dependency2 = dependency2;
+  }
+
+  /* setter injection
+  @Autowired
+  public void setDependency1(Dependency1 dependency1) {
+
+    System.out.println("Setter Injection - setDependency1");
+    this.dependency1 = dependency1;
+  }
+
+  @Autowired
+  public void setDependency2(Dependency2 dependency2) {
+
+    System.out.println("Setter Injection - setDependency2");
+    this.dependency2 = dependency2;
+  }
+  */
+
+  public String toString() {
+
+    return "Using " + dependency1 + " and " + dependency2;
+  }
+}
+
+// Dependency1
+@Component
+class Dependency1 {
+
+}
+
+// Dependency2
+@Component
+class Dependency2 {
+
+}
+
+@Configuration
+@ComponentScan
+public class DepInjectionLauncherApplication {
+
+  public static void main(String[] args) {
+
+    var context =
+        new AnnotationConfigApplicationContext(
+            DepInjectionLauncherApplication.class
+        );
+
+      Arrays.stream( context.getBeanDefinitionNames() )
+          .forEach( System.out::println );
+
+    System.out.println(context.getBean( YourBusinessClass.class ));
+  }
+}
